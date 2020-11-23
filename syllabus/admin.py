b35c1b0cbfdf2e051d6a4c6072533843fc39d8a3
Ted <tedbanken@gmail.com>
2020-11-23 02:38:30 -0600

    added user models, registered them in admin

--- syllabus/admin.py
+++ syllabus/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from syllabus.models import TA, Instructor
 
 # Register your models here.
+
+
+# class RegTA(admin.ModelAdmin):
+#     fields = ['username']
+
+
+admin.site.register(TA)
+admin.site.register(Instructor)
--- syllabus/models.py
+++ syllabus/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    #user,psword,firstlastname,course foreignkey,section foreignkey(?)
+    pass
+
+
+
+class TA(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=20)
+    # course foreignkey??
+
+
+class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=20)
