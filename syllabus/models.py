02f06c01ae953d50e7c3d60a58fd38a9e5da0f07
Ted <tedbanken@gmail.com>
2020-11-24 13:23:43 -0600

    created Login view, Home view, and related homepage for each user

--- syllabus/models.py
+++ syllabus/models.py
@@ -8,20 +8,39 @@ from django.db import models
 class MyUser(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.username
+
+
+class Admin(MyUser):
+    class Meta:
+        verbose_name = "Admin"
+        verbose_name_plural = "Admins"
+
+
+class TA(MyUser):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     office = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
-    email = models.CharField(max_length=20)
     officeHours = models.CharField(max_length=20)
-
-
-class TA(MyUser):
-    pass
+    class Meta:
+        verbose_name = "TA"
+        verbose_name_plural = "TAs"
 
 
 class Instructor(MyUser):
-    pass
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    office = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    officeHours = models.CharField(max_length=20)
+    class Meta:
+        verbose_name = "Instructor"
+        verbose_name_plural = "Instructors"
 
 
 # class Syllabus(models.Model):
